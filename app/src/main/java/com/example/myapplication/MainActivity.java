2ef85d13defe396bc4a94607b6680ebd785e7957
vidatonkova <vidatonkova@ufl.edu>
2020-05-07 20:19:14 -0400

    some nav drawer updates

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Build;
@@ -13,19 +16,36 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 import java.util.ArrayList;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     ArrayList<String> list = new ArrayList<String>();
     ArrayAdapter adapter;
+    //menu buttons
+/*
+home == has alerts and such
+employee list == when you click on an employee-> shows detailed status
+register device
+my account == update password and update profile
+help
+logout
+ */
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+       //this is our listview
         listView = (ListView)findViewById(R.id.list_view);
 
         list.add("burrito1");
@@ -37,18 +57,7 @@ public class MainActivity extends AppCompatActivity {
         list.add("burrito");
         list.add("burrito");
         list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
-        list.add("burrito");
+
 
         adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
@@ -64,6 +73,19 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        //this is the navigation drawer menu
+        /*
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();*/
+
+        //these are the buttons
         configureNextButton();
         configureRobinButton();
     }
