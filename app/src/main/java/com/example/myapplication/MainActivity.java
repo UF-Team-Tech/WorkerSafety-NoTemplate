081bc3f555f2b96f7202acab6212cd669764232d
gvoyts <gvoytseshko@ufl.edu>
2020-05-11 16:25:58 -0400

    Revert "some nav drawer updates"
    This reverts commit 2ef85d13

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,10 +1,7 @@
 package com.example.myapplication;
 
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Build;
@@ -16,36 +13,19 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import com.google.android.material.navigation.NavigationView;
-
 import java.util.ArrayList;
-
-
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
     ArrayList<String> list = new ArrayList<String>();
     ArrayAdapter adapter;
-    //menu buttons
-/*
-home == has alerts and such
-employee list == when you click on an employee-> shows detailed status
-register device
-my account == update password and update profile
-help
-logout
- */
-    DrawerLayout drawerLayout;
-    NavigationView navigationView;
-    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       //this is our listview
         listView = (ListView)findViewById(R.id.list_view);
 
         list.add("burrito1");
@@ -57,7 +37,18 @@ logout
         list.add("burrito");
         list.add("burrito");
         list.add("burrito");
-
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
+        list.add("burrito");
 
         adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
@@ -73,19 +64,6 @@ logout
         });
 
 
-        //this is the navigation drawer menu
-        /*
-        drawerLayout = findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.nav_view);
-        toolbar = findViewById(R.id.toolbar);
-
-        setSupportActionBar(toolbar);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();*/
-
-        //these are the buttons
         configureNextButton();
         configureRobinButton();
     }
